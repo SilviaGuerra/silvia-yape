1ad8e6f8bbe6c4994f34dcb1d452bbf595d6808c
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-07-19 12:45:32 -0500

    mejoramiento de validacion de telefono

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,27 @@
 var cargarPagina = function(){
   $('.carousel.carousel-slider').carousel({fullWidth: true});
   $("#ingreso-numero").keypress(ingresoNumero);
+  $(".checkbox_terminos").click(aceptoTerminos);
 }
 
 var ingresoNumero = function(e){
   var ascii = e.keyCode;
   var cuenta = $("#icon_telephone").val().length;
 
-  if(cuenta <= 10 || ascii >= 46) {
+  if(cuenta == 9 && ascii >= 46) {
+    $(".btn-continuar").removeAttr("disabled");
+  }else if(cuenta <= 10 && ascii >= 59) {
     $(".btn-continuar").attr("disabled", true);
   }
+}
 
-  if(cuenta == 9 || ascii == 59) {
+var aceptoTerminos = function(){
+  var checkbox = $(".checkbox_terminos");
+
+  if(checkbox == false){
     $(".btn-continuar").removeAttr("disabled");
+  } else {
+    $(".btn-continuar").attr("disabled", true);
   }
 }
 
