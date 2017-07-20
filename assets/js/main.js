59a746ea1c39c2b66cddbeca5ae0353565b04de4
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-07-20 14:49:37 -0500

    modal con numero aleatorio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
+var api = {
+  url: 'http://localhost:3000/api/registerNumber'
+}
+
 var cargarPagina = function(){
   $('.carousel.carousel-slider').carousel({fullWidth: true});
   $("#ingreso-numero").keypress(ingresoNumero);
   $(".checkbox_terminos").click(ingresoNumero);
+  $(".btn-continuar").click(generarCodigoAleatorio);
+  // $(".btn-continuar").click(metodoPost);
+
 }
 
 var ingresoNumero = function(e){
@@ -21,7 +28,23 @@ var ingresoNumero = function(e){
   if(cuenta == 10 && !checkbox.checked){
     $(".btn-continuar").removeAttr("disabled");
   }
+}
 
+var generarCodigoAleatorio = function (e) {
+  $('.modal').modal();
+
+  var digito1=Math.floor(Math.random()*10).toString();
+  var digito2 = Math.floor(Math.random()*10).toString();
+  var digito3 = Math.floor(Math.random()*10).toString();
+  var codigoAleatorio = $("#codigoAleatorio");
+
+  localStorage.digito1=digito1;
+  localStorage.digito2=digito2;
+  localStorage.digito3=digito3;
+
+  var tuCodigo = $("<p>");
+  tuCodigo.text(digito1 + digito2 + digito3);
+  codigoAleatorio.append(tuCodigo);
 }
 
 $(document).ready(cargarPagina);
